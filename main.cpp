b1824ea5e102d78f63090176191f3ea029f678c2
Daniel <dannyp95@gmail.com>
2020-09-07 13:18:04 +0200

    changes to main

--- main.cpp
+++ main.cpp
@@ -176,8 +176,6 @@ int main(int argc, char* argv[]) {
     int b;
     int c;
 
-    char task;
-
     if (argc < 4) {
         //If no commands are specified, exit program
         //Could instead provide a standard set of values
@@ -188,88 +186,122 @@ int main(int argc, char* argv[]) {
         a = std::atoi(argv[1]);
         b = std::atoi(argv[2]);
         c = std::atoi(argv[3]);
-
-    askForInput:
-        std::cout << "Please input what task you want to solve (valid tasks: b, c, d, e, f and 0 for exit): ";
-        std::cin >> task;
+        char task = ' ';
 
         std::clock_t start, finish;
 
-        switch (task) {
-        //TODO create general case were the user can choose n, filename themselves
-        //TODO declare a global n array for all tasks?
-        case 'b': {
-            int n[3] = { 10, 100, 1000 };
-            for (int i = 0; i < 3; i++) {
-                double h = 1.0 / (n[i] + 1);
+        std::cout << "Valid tasks are b, c, d, or e, f for custom and 0 to exit. " << std::endl;
+        while (task != '0') {
+            std::cout << "Insert task: ";
+            std::cin >> task;
 
-                //Timing the execution of the algorithm
-                start = std::clock();
-                double* v = generalSolver(n[i], h, a, b, c);
-                finish = std::clock();
+            switch (task) {
+            //TODO declare a global n array for all tasks?
+            case 'b': {
+                int n[3] = { 10, 100, 1000 };
+                for (int i = 0; i < 3; i++) {
+                    double h = 1.0 / (n[i] + 1);
 
-                //double* u;
-                double* u = analyticalSolution(n[i], h);
+                    //Timing the execution of the algorithm
+                    start = std::clock();
+                    double* v = generalSolver(n[i], h, a, b, c);
+                    finish = std::clock();
 
-                double execution_time = double(finish - start) / double(CLOCKS_PER_SEC);
-                std::cout << "Execution time for n =  " << n[i] << " is " << std::fixed << execution_time
-                    << std::setprecision(7) << " seconds" << std::endl;
+                    double* u = analyticalSolution(n[i], h);
 
-                writeToFile("task_b", n[i], v, u);
+                    double execution_time = double(finish - start) / double(CLOCKS_PER_SEC);
+                    std::cout << "Execution time for n = " << n[i] << " is " << std::fixed << std::setprecision(4) 
+                        << execution_time*1000 << "ms" << std::endl;
+
+                    writeToFile("task_b", n[i], v, u);
+
+                    delete[] v, u;
+                    v, u = NULL;
+                }
+                std::cout << "Task b has been completed \n" << std::endl;
+                break;
+            }
+            case 'c': {
+                //TODO: Create a specialized algo
+                //Similar structure as task b
+                int n[5] = { 10, 100, 1000, 10000, 1000000 };
+                std::cout << "Not yet implemented" << std::endl;
+                exit(1);
+            }
+            case 'd': {
+                //TODO not finished yet
+                int n = int(pow(10, 7));
+                double h = 1.0 / (n + 1);
+
+                //Can use specialized algorithm instead
+                double* v = generalSolver(n, h, a, b, c);
+                double* u = analyticalSolution(n, h);
+
+                //The realtive error
+                double* rel_err = new double[n];
+                for (int i = 0; i < n; i++) {
+                    rel_err[i] = std::log10(std::abs((v[i] - u[i]) / u[i]));
+                }
+                std::cout << "Writing to file, this might take a while." << std::endl;
+                writeToFile("task_c", n, v, u, rel_err);
+
+                std::cout << "Not completely implemented yet" << std::endl;
 
                 delete[] v, u;
                 v, u = NULL;
+                break;
             }
-            std::cout << "Task b completed" << std::endl;
-            break;
-        }
-        case 'c': {
-            //TODO: Create a specialized algo
-            //Similar structure as task b
-            int n[5] = { 10, 100, 1000, 10000, 1000000 };
-            std::cout << "Not yet implemented" << std::endl;
-            exit(1);
-        }
-        case 'd': {
-            //TODO not finished yet
-            int n = int(pow(10, 7));
-            double h = 1.0 / (n + 1);
-
-            //Can use specialized algorithm instead
-            double* v = generalSolver(n, h, a, b, c);
-            double* u = analyticalSolution(n, h);
-
-            //The realtive error
-            double* rel_err = new double[n];
-            for (int i = 0; i < n; i++) {
-                rel_err[i] = std::log10(std::abs((v[i] - u[i]) / u[i]));
+            case 'e': {
+                //Use armadillo or make our own implementation of LU decomp. ?
+                int n[3] = { 10, 100, 1000 };
+                std::cout << "Not yet implemented" << std::endl;
+                exit(1);
             }
-            std::cout << "Writing to file, this might take a while." << std::endl;
-            writeToFile("task_c", n, v, u, rel_err);
+            case 'f': {
+                int n; std::string filename;
+                std::cout << "Enter value for n, filename: ";
+                std::cin >> n >> filename;
+                std::cout << "\n";
+                std::cout << "You entered: " << n << ", " << filename << std::endl;
 
-            std::cout << "Not completely implemented yet" << std::endl;
+                double h = 1.0 / (n + 1);
 
-            delete[] v, u;
-            v, u = NULL;
-            break;
-        }
-        case 'e': {
-            //Use armadillo or make our own implementation of LU decomp. ?
-            int n[3] = { 10, 100, 1000 };
-            std::cout << "Not yet implemented" << std::endl;
-            exit(1);
-        }
-        case '0': {
-            exit(1);
-        }
-        default: {
-            std::cout << "Error, must insert valid character!" << std::endl;
-            //Jumps back to cin
-            goto askForInput;
-        }
-        }
+                //Timing the execution of the algorithm
+                start = std::clock();
+                double* v = generalSolver(n, h, a, b, c);
+                finish = std::clock();
 
+                double* u = analyticalSolution(n, h);
+
+                double execution_time = double(finish - start) / double(CLOCKS_PER_SEC);
+                std::cout << "Execution time for n = " << n << " is " << std::fixed << std::setprecision(4)
+                    << execution_time * 1000 << "ms" << std::endl;
+
+                std::cout << "Writing data to file..." << std::endl;
+                writeToFile(filename, n, v, u);
+
+                delete[] v, u;
+                v, u = NULL;
+
+                std::cout << "Task completed! \n" << std::endl;
+                break;
+            }
+            default: {
+                if (task == '0') {
+                    std::cout << "Exiting..." << std::endl;
+                }
+                else {
+                    std::cout << "Error, must insert valid character!" << std::endl;
+                    break;
+                }
+            }
+
+            }
+
+        }
     }
+
+
 
     return 0;
 }
