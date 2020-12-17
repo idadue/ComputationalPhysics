ed0b132757d71b79c8322f1a47f9cec15344adcb
idadu <idadu@student.matnat.uio.no>
2020-12-17 11:03:30 +0100

    Update test cases

--- project5/src/SIRS_vital_dynamics_test.cpp
+++ project5/src/SIRS_vital_dynamics_test.cpp
@@ -1,5 +1,5 @@
 // An example of the use of methods in the ODEsolver class to numerically
-// solve an SIRS model
+// solve an SIRS model with vital dynamics
 
 #include <algorithm>
 #include <cstdlib>
@@ -13,19 +13,25 @@ int main(int argc, char **argv){
   double time = 0.0;
   double delta_t = 0.0010;
   vector<double> vtime;
-  while (time<=(10.0+delta_t)){
+  while (time<=(40.0+delta_t)){
      vtime.push_back(time);
      time = time + delta_t;
   }
   
   // set parameters and initial values
-  double a = 4.0;
+  double a  = 4.0;
   double b = std::atoi(argv[1]);
   double c = 0.5;
+  double d = 0.003;
+  double d_i = 0.01;
+  double e = 0.0005;
   vector<double> Parameters;
   Parameters.push_back(a);
   Parameters.push_back(b);
   Parameters.push_back(c);
+  Parameters.push_back(d);
+  Parameters.push_back(d_i);
+  Parameters.push_back(e);
 
   double N = 400;
   double I_0 = 100.0;
@@ -36,13 +42,13 @@ int main(int argc, char **argv){
   InitialValues.push_back(I_0);
   InitialValues.push_back(R_0);
   
-  // ModelType==1 is the SIRS model
-  int ModelType = 1;
-  ODEsolver SIRS(InitialValues, Parameters, ModelType);
+  // ModelType==2 is the SIRS model with vital dynamics
+  int ModelType = 2;
+  ODEsolver SIRS_vital(InitialValues, Parameters, ModelType);
 
   // solve the model using the 4th order Runge-Kutta method
   vector<vector<double> > ModelEstimates;
-  SIRS.SimulateModel(vtime
+  SIRS_vital.SimulateModel(vtime
                      ,ModelEstimates);
 
   for (int i=0;i<vtime.size();i++){
